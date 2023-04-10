a1462d20d431413c468eb73b647602e9c0743bee
EaswarDSharma <easwardsharma@gmail.com>
2023-04-10 20:22:37 +0530

    added location

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-
+ 
 export default ({ history }) => {
   return (
     <div>
